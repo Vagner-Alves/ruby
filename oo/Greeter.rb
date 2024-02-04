485db0b98f20532f3a6478c1fead540fe6989acb
vagner-alves <vagneralves997@gmail.com>
2024-02-03 21:01:03 -0300

    update code

--- oo/Greeter.rb
+++ oo/Greeter.rb
@@ -1,16 +1,16 @@
 class Greeter
 
-  def initialize(name = "world")
-    @name = name
-  end
+    def initialize(name = "world")
+      @name = name
+    end
 
-  def say_hi
-    puts("Hi, #{@name}!!!")
-  end
+    def say_hi
+      puts("Hi, #{@name}!!!")
+    end
 
-  def say_bye
-    puts("Bye!, #{@name}, see you soon!")
-  end
+    def say_bye
+      puts("Bye!, #{@name}, see you soon!")
+    end
 end
 
 greeter = Greeter.new("Vagner")
