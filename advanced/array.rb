7f05a3df432c1c9d684ed92363a7196f355221f4
vagner-alves <vagneralves997@gmail.com>
2024-02-04 22:51:34 -0300

    advanced

--- advanced/array.rb
+++ advanced/array.rb
@@ -4,4 +4,13 @@ books.push("a ilha do conhecimento")
 books.push("1984")
 books.push("Trabalho focado")
 
+
 puts books
+
+puts "   "
+
+books.insert(0,"a revolução dos bichos", "a baleia e outros contos")
+puts books
+
+puts "---------------"
+puts "quantidade de livros #{books.count}"
