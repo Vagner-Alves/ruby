120149ae5d591961a7dd54c8c8bbdb83df0be798
vagner-alves <vagneralves997@gmail.com>
2024-02-06 10:36:46 -0300

    hashes

--- advanced/hash.rb
+++ advanced/hash.rb
@@ -1,3 +1,6 @@
 # hashes são um tipo de lista, porém armazena elementos com chave e valor
 # são muito parecidos com os dicionários no python
-hash = Hash.new
+zoo = Hash.new
+
+zoo = {"elephants":5,"hippos":8, "leons":12, "sharks":3, "exotic birds": 100}
+puts zoo
