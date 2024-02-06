e960f995de88e2c4ea4ad0bc613466890daf8a7c
vagner-alves <vagneralves997@gmail.com>
2024-02-06 18:49:58 -0300

    methods

--- advanced/method.rb
+++ advanced/method.rb
@@ -6,3 +6,20 @@ end
 hello
 hello
 hello
+
+# parametros
+
+def my_method(name, surname)
+    puts "Hello! #{name}, #{surname}"
+end
+
+
+my_method("vagner","alves")
+
+# entrada do usuário
+print "digite seu nome: "
+name = gets.chomp
+print "digite seu sobrenome: "
+surname = gets.chomp
+
+my_method(name, surname)
