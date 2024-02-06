fa9f70e89f92ec0870cbed6345e9f9370b10b74b
vagner-alves <vagneralves997@gmail.com>
2024-02-06 11:18:00 -0300

    hashes

--- advanced/hash.rb
+++ advanced/hash.rb
@@ -2,5 +2,8 @@
 # são muito parecidos com os dicionários no python
 zoo = Hash.new
 
-zoo = {"elephants":5,"hippos":8, "leons":12, "sharks":3, "exotic birds": 100}
+zoo = {"elephants":5,"hippos":8, "leons":12, "sharks":3, "exotic-birds": 100}
 puts zoo
+
+puts "modificando o valor"
+puts zoo["hippos"] = 9
