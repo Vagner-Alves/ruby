3d4da6fcd863c08c0a7d36e97e2252a08902f30c
vagner-alves <vagneralves997@gmail.com>
2024-02-08 14:01:10 -0300

    gems

--- advanced/gems.rb
+++ advanced/gems.rb
@@ -1,11 +1,19 @@
 # import some gems here and create a simple algorithm
 
-require os
+require 'Os'
+
 
 def my_os
-    if os.Windows?
-            puts "Meu Sistema Operacional é #{os.Windows}"
-    end
+  if OS.windows?
+        puts "Meu Sistema Operacional é Windows"
+
+  elsif OS.linux?
+        puts "Meu Sistema Operacional é Linux"
+  elsif OS.mac?
+      puts "Meu Sistema Operacional é MacOs"
+  else
+    puts "Não reconheço o Sistema Operacional"
+  end
 end
 
 my_os
