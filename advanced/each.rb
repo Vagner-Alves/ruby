7c30938518decb8a766affe1f1994ef6cb7f94c0
vagner-alves <vagneralves997@gmail.com>
2024-02-10 22:20:58 -0300

    each

--- advanced/each.rb
+++ advanced/each.rb
@@ -11,3 +11,11 @@ jogos = {"bioshock":19, "lego batman": 12, "pubg":0}
 jogos.each do |key,value|
   puts "meus jogos:#{key}"
 end
+
+
+courses = {"aws devops modernization":"devops","github actions":"ci/cd", "ruby":"development",
+"kubernetes learning path":"containers", "Hashicorp terraform associate":"iac"}
+
+courses.each do |key, value|
+  puts "meus cursos de tecnologia: #{key}"
+end
