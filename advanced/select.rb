49dcd1eecc48e0318dfcae6e29ba9b0b6dad48a3
vagner-alves <vagneralves997@gmail.com>
2024-02-11 19:42:41 -0300

    select

--- advanced/select.rb
+++ advanced/select.rb
@@ -1,2 +1,12 @@
 numbers = [-10,-9,-8,-7,-6,-5,
 -4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+
+positives = numbers.select do |posi|
+    posi > 0
+end
+
+puts "\n number arrays"
+puts "#{numbers}"
+
+puts "\n positive numbers only"
+puts "#{positives}"
