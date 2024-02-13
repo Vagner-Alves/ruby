af1ee9c4986ac670f696357a7d331487ceee428a
vagner-alves <vagneralves997@gmail.com>
2024-02-13 00:15:50 -0300

    cpf code

--- advanced/cpf.rb
+++ advanced/cpf.rb
@@ -3,9 +3,14 @@ require "cpf_cnpj"
 print "digite um cpf para verificar se é valído"
 number = gets.chomp
 
-cpf = CPF.new(number)
-if cpf.valid?
-  puts "o CPF #{cpf.formatted} é válido"
-elsif
-  puts "O CPF #{cpf} informado não é válido"
+def check(number)
+  cpf = CPF.new(number)
+  if cpf.valid?
+    puts "o CPF #{cpf.formatted} é válido"
+  elsif
+    puts "O CPF #{cpf} informado não é válido"
+  end
+
 end
+
+check(number)
