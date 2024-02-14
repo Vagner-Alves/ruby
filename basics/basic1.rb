1e083947fc103602ed5f1b7a1786ff3057bc1f22
vagner-alves <vagneralves997@gmail.com>
2024-02-14 11:19:15 -0300

    basic

---
+++ basics/basic1.rb
@@ -0,0 +1,7 @@
+print "informe um número:  "
+number = gets.to_i
+#TODO: print no console a frase com o resultado da operação
+
+resultado = number + 2
+
+puts "O resultado foi: #{resultado}"
---
+++ oo/escope.rb
@@ -0,0 +1,2 @@
+# escopo: define onde uma variável estará disponível dentro do programa
+# em ruby temos 4 tipos disponíveis: locais, globais, classes e instancias.
