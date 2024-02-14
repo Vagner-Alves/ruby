04814037b2522dd98b75fcf581b7479dd24d1743
vagner-alves <vagneralves997@gmail.com>
2024-02-14 11:20:49 -0300

    basic

---
+++ basics/basic2.rb
@@ -0,0 +1,10 @@
+print "informe um número: "
+number = gets.to_i
+num = 25
+
+#TODO: Crie as condições necessárias para verificar se a variável
+#é maior que a constante e print no console a frase
+
+if number > num
+  puts "Está calor!"
+end
