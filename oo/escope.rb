6a33c8e74253bfc6c1f2a790fd5b518f75806aad
vagner-alves <vagneralves997@gmail.com>
2024-02-14 21:20:20 -0300

    variable scopes

--- oo/escope.rb
+++ oo/escope.rb
@@ -1,2 +1,9 @@
 # escopo: define onde uma variável estará disponível dentro do programa
 # em ruby temos 4 tipos disponíveis: locais, globais, classes e instancias.
+
+class Pessoa
+  def nome
+        local = "é acessada somente nesse método"
+        print
+  end
+end
