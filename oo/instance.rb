bfd6862cebe33cc9d7979e6e8d8afd7bacb87085
vagner-alves <vagneralves997@gmail.com>
2024-02-15 13:54:48 -0300

    instance variables

---
+++ oo/instance.rb
@@ -0,0 +1,15 @@
+class User
+    def nome(nome)
+            @nome = nome
+            puts "usuário adicionado"
+            ola
+    end
+
+    def ola
+            puts "olá, #{@nome}"
+    end
+
+end
+
+usuario = User.new
+usuario.nome(:'Vagner')
