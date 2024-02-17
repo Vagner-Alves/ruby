0c1b39a9bf179b311c6e80b805aea612db56c530
vagner-alves <vagneralves997@gmail.com>
2024-02-17 13:23:52 -0300

    enhance code

--- project02/app.rb
+++ project02/app.rb
@@ -4,3 +4,18 @@ require_relative "product"
 product1 = Product.new
 product1.name =  "chocolate cake"
 product1.price = 18
+
+product2 = Product.new
+product2.name = "strawberry cake"
+product2.price = 6
+
+items = []
+
+items.push(product1)
+items.push(product2)
+
+#Market.new(product1.name, product1.price).purchase
+#Market.new(product2.name, product2.price).purchase
+#Market.new(product2.name, product2.price).cart
+
+Market.new(items).purchase
--- project02/market.rb
+++ project02/market.rb
@@ -1,11 +1,20 @@
 
 class Market
-  def initialize(product, price)
-        @product = product
-        @price = price
+  def initialize(items)
+
+          for item in items
+                  @name = item.name
+                  @price = item.price
+          end
   end
 
   def purchase
-        puts "Voce comprou o produto: #{@product} por R$#{@price}"
+        puts "Voce comprou o produto: #{@name} por R$#{@price}"
+  end
+
+  def cart(list)
+        puts "Product list"
+        puts "#{@product}"
+        puts
   end
 end
