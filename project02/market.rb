c775320793af5557713470a3da9dec7b9311fd87
vagner-alves <vagneralves997@gmail.com>
2024-02-16 23:18:52 -0300

    project 02

--- project02/market.rb
+++ project02/market.rb
@@ -2,4 +2,8 @@ class Market
   def initialize(product)
         @product = product
   end
+
+  def purchase
+        puts "Voce comprou o produto: #{@product.name} por R$#{product.price}"
+  end
 end
