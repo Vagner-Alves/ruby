df526fb49e8e22ff0494ee78af4ec510d256e387
vagner-alves <vagneralves997@gmail.com>
2024-02-25 16:16:15 -0300

    lambda

--- closures/lambda.rb
+++ closures/lambda.rb
@@ -1,6 +1,6 @@
 lambda01 = lambda{ puts "1st lambda function"}
 lambda01.call
 
-# outra maneira de criar uma função lambda
+# outra maneira de criar uma função lambda, usando " -> "
 lambda02 = -> {puts 2 + 2}
 lambda02.call
