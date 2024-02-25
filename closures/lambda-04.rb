09a38101ea1f5b721fb378e9300b35cf7366b9c2
vagner-alves <vagneralves997@gmail.com>
2024-02-25 17:19:08 -0300

    lambda

--- closures/lambda-04.rb
+++ closures/lambda-04.rb
@@ -3,3 +3,14 @@
 # * Passar pequenos blocos de código em metódos!
 # * criar closures
 # * funcões de alta ordem
+
+# orgazinando um array!
+
+numbers = [1,5,8,9,10]
+sorted_numbers = numbers.sort{|a,b| a <=> b}
+puts "lista em ordem."
+puts sorted_numbers
+
+even_numbers = numbers.select {|number| number % 2 == 0}
+puts "números primos"
+puts even_numbers
