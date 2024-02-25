7e3c00bbd32a5b98919101d93cf97d97a2da6d9b
vagner-alves <vagneralves997@gmail.com>
2024-02-25 16:41:57 -0300

    lambda

---
+++ closures/lambda-04.rb
@@ -0,0 +1,5 @@
+# quando usar funções lambdas?
+# aqui estão algumas situações em que essas funções brilham!
+# * Passar pequenos blocos de código em metódos!
+# * criar closures
+# * funcões de alta ordem
