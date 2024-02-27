03af3fe6f1c849495d158f329ad62ebe586ccea0
vagner-alves <vagneralves997@gmail.com>
2024-02-27 17:31:00 -0300

    regex

---
+++ regular expressions/regex-01.rb
@@ -0,0 +1,10 @@
+def contains_vowel(str)
+    str =~ /[aeiou]/
+end
+
+contains_vowel('banana')
+
+puts "Do you like cats?" =~ /like/
+
+# vai retorna o indice da primeira ocorrencia da palavra
+#  se achada.. ou nill ( null)
