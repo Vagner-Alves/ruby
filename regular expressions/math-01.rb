7f89de0e7344487510cf73448eed9600491e4f84
vagner-alves <vagneralves997@gmail.com>
2024-02-29 19:04:25 -0300

    math

--- regular expressions/math-01.rb
+++ regular expressions/math-01.rb
@@ -1,2 +1,6 @@
-puts Math.sqrt(9)
+puts Math.sqrt(9) # calcula a raiz quadrada de um dado número!
 puts Math.sqrt(16)
+
+puts Math.log10(100) # calcula o log de 10 na base 100
+
+# Math é uma biblioteca muito importante no ruby!
