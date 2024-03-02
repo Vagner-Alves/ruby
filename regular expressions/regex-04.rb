1d28c0bec1b950e3e182c39502c9a60020db9d50
vagner-alves <vagneralves997@gmail.com>
2024-03-01 21:34:33 -0300

    regex +

--- regular expressions/regex-04.rb
+++ regular expressions/regex-04.rb
@@ -2,3 +2,7 @@ phrase =  "olá, como vai?"
 data = /como/.match(phrase)
 puts data
 puts data.pre_match
+puts data.post_match
+
+# procurar por simbolos especiais como:  ?
+puts /\?/.match('tudo bem?')
