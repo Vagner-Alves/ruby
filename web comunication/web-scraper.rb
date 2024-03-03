ae95f1995b841ded328d1f3f05987690168bc5a4
vagner-alves <vagneralves997@gmail.com>
2024-03-03 14:29:41 -0300

    web scraper

--- web comunication/web-scraper.rb
+++ web comunication/web-scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'net/http'
 
 http = Net::HTTP.new('example.com', 443)
+http.use_ssl = true  # usa https
+
+response = http.get('/')
+
+document = Nokogiri::HTML(response.body)
+h1 = document.at('h1')
+
+puts h1.content
