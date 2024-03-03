6a3109f8d196136ba6fc40c8ff173523829a7caf
vagner-alves <vagneralves997@gmail.com>
2024-03-03 12:11:39 -0300

    code fix

--- web comunication/web-get.rb
+++ web comunication/web-get.rb
@@ -2,6 +2,6 @@ require "net/http"
 
 sample = Net::HTTP.get('www.google.com','index')
 
-File.open('google.html','w') do |line|
+File.open('C:\Users\Notebook do Vagner\Documents\DIO\formacao ruby\ruby\web comunication\files\googleerr.html','w') do |line|
     line.puts(sample)
 end
